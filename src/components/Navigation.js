cb85ab7123aa1a976edde3d135541a48aeeb1504
sachinarbhat <sachinarbhat@gmail.com>
2022-01-05 23:07:03 +0530

    First commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navigation() {
+    return (
+        <nav className="navbar navbar-light text-center bg-warning">
+            <div className="container-fluid">
+                <div className="navbar-brand">
+                    <img src="https://cdn2.iconfinder.com/data/icons/food-restaurant-1/128/flat-11-512.png" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+                    <b>Dessert's Recipe</b>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
